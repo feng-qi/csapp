6693b0bee85ccba8b2fb75cd89455d02880b14bd
fengqi <fengqi@microtrust.com.cn>
2017-03-07 20:07:03 +0800

    Rio: Robust io

---
+++ chapter10/rio/cpfile.c
@@ -0,0 +1,16 @@
+#include "./rio.h"
+
+#define MAXLINE 1024
+
+int main(int argc, char *argv[])
+{
+    int   n;
+    rio_t rio;
+    char  buf[MAXLINE];
+
+    rio_readinitb(&rio, STDIN_FILENO);
+    while ((n = rio_readlineb(&rio, buf, MAXLINE)) != 0)
+        rio_writen(STDOUT_FILENO, buf, n);
+
+    return 0;
+}
